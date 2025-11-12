f8b23964fc7c51e8a2f8ad63db5b5d5f8297aba4
lokesh0221 <114285647+lokesh0221@users.noreply.github.com>
2025-11-12 20:50:58 +0530

    Update api.py

--- api.py
+++ api.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 
 client = OpenAI(
   base_url="https://openrouter.ai/api/v1",
-  api_key="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",
+  api_key="",
 )
 
 completion = client.chat.completions.create(
